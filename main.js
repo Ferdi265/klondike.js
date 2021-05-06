76420fa6eaf7cf0f6a8767aa3ea69a8312595174
Ferdinand Bachmann <theferdi265@gmail.com>
2021-05-06 19:40:11 +0200

    switch to OO pattern

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
-const canvas_create = () => {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-
-    return {
-        dom: canvas,
-        ctx: ctx
+const Listeners = (target) => {
+    const private = {
+        target,
+        handlers: []
     };
+
+    const self = {
+        register: (event, func, capture) => {
+            if (capture === undefined) capture = false;
+
+            private.handlers.push({ event, func, capture });
+            private.target.addEventListener(event, func, capture);
+        },
+        deregister: (event, func, capture) => {
+            private.handlers = private.handlers.filter((handler) => {
+                if (event !== undefined && event !== handler.event) return true;
+                if (func !== undefined && func !== handler.func) return true;
+                if (capture !== undefined && capture !== handler.capture) return true;
+
+                private.target.removeEventListener(handler.event, handler.func, handler.capture);
+                return false;
+            });
+        },
+    };
+
+    return self;
 };
 
-const canvas_update_size = (canvas) => {
-    canvas.dom.width = window.innerWidth;
-    canvas.dom.height = window.innerHeight;
-};
+const Canvas = () => {
+    const dom = document.createElement("canvas");
+    const ctx = dom.getContext("2d");
 
-const canvas_register = (canvas) => {
-    document.body.innerHTML = "";
-    document.body.appendChild(canvas.dom);
+    const private = {
+        listeners: Listeners(dom)
+    };
 
-    document.addEventListener("resize", () => canvas_update_size(canvas));
+    const self = {
+        dom,
+        ctx,
+
+        update_size: () => {
+            self.dom.width = window.innerWidth;
+            self.dom.height = window.innerHeight;
+        },
+
+        register: () => {
+            document.body.innerHTML = "";
+            document.body.appendChild(canvas.dom);
+
+            private.listeners.register("resize", self.update_size);
+        },
+
+        deregister: () => {
+            private.listeners.deregister();
+        }
+    };
+
+    return self;
 };
 
 /* global */ canvas = null;
 
 const initialize = () => {
-    canvas = canvas_create();
-    canvas_update_size(canvas);
-    canvas_register(canvas);
+    canvas = Canvas();
+    canvas.update_size();
+    canvas.register();
 };
 
-window.addEventListener("DOMContentLoaded", () => initialize());
+window.addEventListener("DOMContentLoaded", initialize);
